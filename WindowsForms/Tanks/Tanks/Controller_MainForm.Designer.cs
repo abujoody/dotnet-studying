2fc0a28f954203dd820e396013232cdada9f7bfb
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2011-10-31 19:13:49 +0000

    "Tanks" project changed. MVC pattern inserted.

--- WindowsForms/Tanks/Tanks/Controller_MainForm.Designer.cs
+++ WindowsForms/Tanks/Tanks/Controller_MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tanks
 {
-    partial class MainForm
+    partial class Controller_MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Controller_MainForm));
             this.SuspendLayout();
             // 
             // MainForm
