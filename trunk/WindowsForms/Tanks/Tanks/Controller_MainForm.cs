6553d8a54d6301ab018276017025f6d955dfcae7
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2011-10-31 19:35:55 +0000

    "Tanks" project changed. Reading of command line arguments added.

--- trunk/WindowsForms/Tanks/Tanks/Controller_MainForm.cs
+++ trunk/WindowsForms/Tanks/Tanks/Controller_MainForm.cs
@@ -11,9 +11,18 @@ namespace Tanks
 {
     public partial class Controller_MainForm : Form
     {
+        const int defSizeField = 260;
+        const int defAmountTanks = 5;
+        const int defAmountApples = 5;
+        const int defSpeedGame = 40;
+
         View view;
 
-        public Controller_MainForm()
+        public Controller_MainForm() : this(defSizeField) {}
+        public Controller_MainForm(int sizeField) : this(sizeField, defAmountTanks) {}
+        public Controller_MainForm(int sizeField, int amountTanks) : this(sizeField, amountTanks, defAmountApples) {}
+        public Controller_MainForm(int sizeField, int amountTanks, int amountApples) : this(sizeField, amountTanks, amountApples, defSpeedGame) {}
+        public Controller_MainForm(int sizeField, int amountTanks, int amountApples, int speedGame)
         {
             InitializeComponent();
 
