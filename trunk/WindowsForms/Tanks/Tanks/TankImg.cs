cd845c8b29bc2560115ea594193dd20e61b5d207
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2011-11-02 19:16:32 +0000

    "Tanks" project changed. 1. Images for different directions added. 2. Proper rendering of different directions implemented.

--- trunk/WindowsForms/Tanks/Tanks/TankImg.cs
+++ trunk/WindowsForms/Tanks/Tanks/TankImg.cs
@@ -8,12 +8,33 @@ namespace Tanks
 {
     class TankImg
     {
-        Image img = Properties.Resources.Tank;
+        Image img0_1 = Properties.Resources.Tank0_1;
+        Image img01 = Properties.Resources.Tank01;
+        Image img10 = Properties.Resources.Tank10;
+        Image img_10 = Properties.Resources.Tank_10;
 
-        public Image Img
+        public Image Img0_1
         {
-            get { return img; }
-            set { img = value; }
+            get { return img0_1; }
+            set { img0_1 = value; }
+        }
+
+        public Image Img01
+        {
+            get { return img01; }
+            set { img01 = value; }
+        }
+
+        public Image Img10
+        {
+            get { return img10; }
+            set { img10 = value; }
+        }
+
+        public Image Img_10
+        {
+            get { return img_10; }
+            set { img_10 = value; }
         }
     }
 }
