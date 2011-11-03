6f4f71c55228954c280098a67937c8397765c053
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2011-11-03 13:06:09 +0000

    "Tanks" project changed. Packman added (behavior (code), resources, but not rendering).

--- trunk/WindowsForms/Tanks/Tanks/Sources/Interfaces/ITurnAround.cs
+++ trunk/WindowsForms/Tanks/Tanks/Sources/Interfaces/ITurnAround.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace Tanks
 {
-    interface ITurn
+    interface ITurnAround
     {
-        void Turn();
+        void TurnAround();
     }
 }
---
+++ trunk/WindowsForms/Tanks/Tanks/Sources/Model/PackmanImg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Tanks.Sources.Model
+{
+    class PackmanImg
+    {
+        Image[] up = new Image[] { 
+            Properties.Resources.PackmanImg0_1,
+            Properties.Resources.PackmanImg0_1I,
+            Properties.Resources.PackmanImg0_1II,
+            Properties.Resources.PackmanImg0_1III,
+            Properties.Resources.PackmanImg0_1IV };
+
+        Image[] down = new Image[] { 
+            Properties.Resources.PackmanImg01,
+            Properties.Resources.PackmanImg01I,
+            Properties.Resources.PackmanImg01II,
+            Properties.Resources.PackmanImg01III,
+            Properties.Resources.PackmanImg01IV };
+
+        Image[] right = new Image[] { 
+            Properties.Resources.PackmanImg10,
+            Properties.Resources.PackmanImg10I,
+            Properties.Resources.PackmanImg10II,
+            Properties.Resources.PackmanImg10III,
+            Properties.Resources.PackmanImg10IV };
+
+        Image[] left = new Image[] { 
+            Properties.Resources.PackmanImg_10,
+            Properties.Resources.PackmanImg_10I,
+            Properties.Resources.PackmanImg_10II,
+            Properties.Resources.PackmanImg_10III,
+            Properties.Resources.PackmanImg_10IV };
+
+        public Image[] Up
+        {
+            get { return up; }
+        }
+
+        public Image[] Down
+        {
+            get { return down; }
+        }
+
+        public Image[] Right
+        {
+            get { return right; }
+        }
+
+        public Image[] Left
+        {
+            get { return left; }
+        }
+    }
+}
