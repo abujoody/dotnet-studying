d119ec9b2351ee8af1482de7dd66078707f9180d
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-06 21:13:57 +0000

    Test application updated. Generating classes using class diagram.

---
+++ Troelsen/Testing/Testing/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testing
+{
+    public class Car
+    {
+        /// <summary>
+        /// Added via Class Designer
+        /// </summary>
+        public string petname;
+    }
+}
