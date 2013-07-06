836d97d2ee72dde96b3380ee82ae7b6a7b8be76c
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-06 20:40:05 +0000

    Test application updated. Minor changes to test refactoring features.

--- Troelsen/Testing/Testing/Program.cs
+++ Troelsen/Testing/Testing/Program.cs
@@ -6,10 +6,27 @@ namespace Testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing! 1, 2, 3");
+            // V1
+            /*Console.WriteLine("Testing! 1, 2, 3");
 
             Message msg = new Message();
-            msg.speak();
+            msg.speak();*/
+
+            // V2
+            ConfigureCUI();
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureCUI()
+        {
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("***********************************");
+            Console.WriteLine("**** Welcome to My Rocking App ****");
+            Console.WriteLine("***********************************");
+            Console.BackgroundColor = ConsoleColor.Black;
         }
     }
 }
