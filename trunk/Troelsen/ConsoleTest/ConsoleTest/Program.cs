876557e726aafb3772f367802b4eceb43108860e
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-07 14:52:41 +0000

    Console Test. IO test.

--- trunk/Troelsen/ConsoleTest/ConsoleTest/Program.cs
+++ trunk/Troelsen/ConsoleTest/ConsoleTest/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleTest
         {
             //Test1();
 
-
+            GetUserData();
         }
 
         private static void Test1()
@@ -39,6 +39,22 @@ namespace ConsoleTest
             Console.WindowLeft = 25;
 
             Console.ReadLine();
+        }
+
+        private static void GetUserData()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            string userAge = Console.ReadLine();
+
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Hello {0}! You are {1} years old",
+                userName, userAge);
+
+            Console.ForegroundColor = prevColor;
         }
     }
 }
