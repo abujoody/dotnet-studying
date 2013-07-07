3b24839d1242304f40b10de49983d7efb16dabc8
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-07 10:24:03 +0000

    Console Test. Basic methods test.

--- trunk/Troelsen/ConsoleTest/ConsoleTest/Program.cs
+++ trunk/Troelsen/ConsoleTest/ConsoleTest/Program.cs
@@ -10,6 +10,35 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
+            //Test1();
+
+
+        }
+
+        private static void Test1()
+        {
+            Console.Beep();
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.ReadLine();
+
+            Console.Title = "Hehehe";
+            Console.BufferHeight = 100;
+            Console.WindowWidth = 100;
+
+            Console.ReadLine();
+
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 50;
+
+            Console.ReadLine();
+
+            Console.WindowTop = 25;
+            Console.WindowLeft = 25;
+
+            Console.ReadLine();
         }
     }
 }
