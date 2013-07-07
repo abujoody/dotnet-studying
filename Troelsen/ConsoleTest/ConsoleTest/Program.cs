295ce026e4d78a656b446c700be5041a60d0ca60
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-07 16:43:34 +0000

    Console Test. Console Test. Object methods 3. .

--- Troelsen/ConsoleTest/ConsoleTest/Program.cs
+++ Troelsen/ConsoleTest/ConsoleTest/Program.cs
@@ -44,6 +44,21 @@ namespace ConsoleTest
             Console.WriteLine("double.NegativeInfinity: {0}",
             double.NegativeInfinity);
             Console.WriteLine();
+
+            Console.WriteLine("bool.FalseString: {0} ", bool.FalseString);
+            Console.WriteLine("bool.TrueString: {0}", bool.TrueString);
+
+            Console.WriteLine("\n=> char type Functionality:");
+            char myChar = 'а';
+            Console.WriteLine("char.IsDigit ('а') : {0}", char.IsDigit(myChar));
+            Console.WriteLine("char.IsLetter ('а') : {0}", char.IsLetter(myChar));
+            Console.WriteLine("char.IsWhiteSpace(\"Hello There\", 5): {0}",
+            char.IsWhiteSpace("Hello There", 5));
+            Console.WriteLine("char.IsWhiteSpace(\"Hello There\", 6): {0}",
+            char.IsWhiteSpace("Hello There", 6));
+            Console.WriteLine("char.IsPunctuation('?'): {0}",
+            char.IsPunctuation('?'));
+            Console.WriteLine();
         }
 
         private static void ObjectTest()
