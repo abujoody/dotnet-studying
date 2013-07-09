737f3101de26c04101eb82bd2a39715064809280
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-09 21:35:40 +0000

    Console Test. Console Test. Arrays + diff.

--- Troelsen/Console2/Console2/Program.cs
+++ Troelsen/Console2/Console2/Program.cs
@@ -6,18 +6,41 @@ using System.Threading.Tasks;
 
 namespace Console2
 {
+    struct testArr { };
+
     class Program
     {
         static void Main(string[] args)
         {
-            VarTest();
+            /*VarTest();
 
             int a, b = 0;
             MetTest(out a, ref b);
 
             ParamsTest(7, 777, 877);
 
-            NamedArgs(b: 55, c: "str", a: 666);
+            NamedArgs(b: 55, c: "str", a: 666);*/
+
+            Parent[] testArr = new Parent[] { new Parent(), new Child() };
+
+            int[,] x = new int [2, 2];
+            int[][][][] y = new int[2][][][];
+            int[,] myArray = new int[4,2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+
+            int[][] myJaggedArray = new int[3][] 
+                        {
+                           new int[] {1,3,5,7,9},
+                           new int[] {0,2,4,6},
+                           new int[] {11,22}
+                        };
+
+            int[] t1 = { 7, 4 };
+            int[] t2 = new int[3];
+            t1.CopyTo(t2, 0);
+
+            Array.Sort(t2);
+
+            Console.WriteLine("{0},{1},{2}", t2[0], t2[1], t2[2]);
         }
 
         private static void VarTest()
