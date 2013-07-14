3ec60efdb91883797c0d3f862c93945f94283abf
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-14 11:29:32 +0000

    Modificator "Static".

--- Troelsen/SimpleClassExample/SimpleClassExample/Program.cs
+++ Troelsen/SimpleClassExample/SimpleClassExample/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace SimpleClassExample
 {
+    static class Test
+    {
+        public static int i;// = 4;
+
+        /*public Test()
+        {
+            Console.WriteLine("ctor");
+            //i = 666;
+        }*/
+
+        static Test()
+        {
+            Console.WriteLine("static ctor");
+            i = 7777;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            //Test ts = new Test();
+
+            Console.WriteLine(Test.i);
         }
     }
 }
