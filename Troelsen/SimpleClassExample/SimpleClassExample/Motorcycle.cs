5a7d3eed6ee104d777d8955f1dba763797f44f67
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-14 09:54:42 +0000

    Test classes.

---
+++ Troelsen/SimpleClassExample/SimpleClassExample/Motorcycle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Motorcycle
+    {
+        public int driverIntensity;
+        public void PopAWheely()
+        {
+            for (int i = 0; i <= driverIntensity; ++i)
+            {
+                Console.WriteLine("Yeeeeeee Haaaaaeewww!");
+            }
+        }
+        
+        // Вернуть стандартный конструктор, который будет устанавливать
+        // для всех членов данных стандартные значения.
+        public Motorcycle() { }
+
+        // Специальный конструктор,
+        public Motorcycle(int intensity)
+        {
+            driverIntensity = intensity;
+        }
+
+        // Новые члены для представления имени водителя,
+        public string name;
+        public void SetDriverName(string name)
+        { this.name = name; }
+    }
+}
