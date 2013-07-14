8d04fc72978109eb4cb94a7fd9b5d1d97019ad12
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-14 20:55:50 +0000

    Encapsulation.

--- trunk/Troelsen/SimpleClassExample/SimpleClassExample/Program.cs
+++ trunk/Troelsen/SimpleClassExample/SimpleClassExample/Program.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace SimpleClassExample
 {
-    static class Test
+    class Test
     {
+        protected internal enum XY { X, Y };
+
         public static int i;// = 4;
 
         /*public Test()
@@ -22,6 +24,20 @@ namespace SimpleClassExample
             i = 7777;
         }
     }
+
+    class Point
+    {
+        //Point xx = new Point(); - very bad idea
+
+        public const int ttt = 555;
+        public readonly int rrr = 33;
+
+        public Point() { rrr = 44; /*ttt = 3; - bad idea */ }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -29,6 +45,18 @@ namespace SimpleClassExample
             //Test ts = new Test();
 
             Console.WriteLine(Test.i);
+
+            Test.XY ttt = Test.XY.X;
+
+            Car cr = new Car();
+            cr.Pet = "Hehehe";
+            Console.WriteLine(cr.Pet);
+            cr.Pet2 = "Hehehe2222";
+            Console.WriteLine(cr.Pet2);
+
+            Point pt = new Point { X = 12, Y = 15 };
+
+            Console.WriteLine(Point.ttt);
         }
     }
 }
