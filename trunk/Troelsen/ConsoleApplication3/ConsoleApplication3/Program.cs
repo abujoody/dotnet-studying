a3098930b7fdc31157083deba788259ac1cf58ba
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-17 20:02:23 +0000

    Console 3

---
+++ trunk/Troelsen/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string test = "dd";
+                object hh = new int[5];
+                test = (string)hh;
+                Console.WriteLine(test);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("!!!!!!!!!!!!!!!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
