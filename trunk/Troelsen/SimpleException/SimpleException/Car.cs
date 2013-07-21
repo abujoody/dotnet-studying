8a4e81aa0322cc864db7f0a3ebed538d2477b027
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-21 17:50:50 +0000

    SimpleException project.

---
+++ trunk/Troelsen/SimpleException/SimpleException/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // Constants
+        public const int MaxSpeed = 100;
+
+        // Car properties
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is car out of service?
+        private bool carIsDead;
+
+        // radio in the car
+        private Radio theMusicBox = new Radio();
+
+        // constructors
+        public Car() {}
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                }
+                else
+                    Console.WriteLine("=> Curr speed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
