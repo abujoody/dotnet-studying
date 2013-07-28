673dd3a5b72d3e51ad65b804e7fcb0f299900bac
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-28 20:25:43 +0000

    Enumerators.

---
+++ Troelsen/YieldTest/YieldTest/Garage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldTest
+{
+    class Garage : IEnumerable
+    {
+        string[] cars = new string[] { "Test1", "Test1", null };
+
+        // interface implementation
+        public IEnumerator GetEnumerator()
+        {
+            return cars.GetEnumerator();
+        }
+
+        // named iterator
+        public IEnumerable NamedEnumerator()
+        {
+            yield return "sdf";
+
+            if (5 > 3)
+                yield return "777";
+
+            if ("777".Length > 8)
+                Console.WriteLine("#######");
+
+            yield break;
+
+            yield return "XXXXXXXXXXXXXXXX";
+        }
+    }
+}
