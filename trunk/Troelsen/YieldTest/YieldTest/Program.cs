a8816c132e6541a47cd919693a3c64c47ba25246
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-28 20:25:43 +0000

    Enumerators.

---
+++ trunk/Troelsen/YieldTest/YieldTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garage g = new Garage();
+            foreach (string car in g)
+            {
+                string carName = car ?? "no car";
+                Console.WriteLine(carName);
+            }
+
+            Console.WriteLine("\n\n\n\n");
+
+            foreach (string car in g.NamedEnumerator())
+            {
+                string carName = car ?? "no car";
+                Console.WriteLine(carName);
+            }
+        }
+    }
+}
