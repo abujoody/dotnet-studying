22b39afd261034b8b8756ac921d907c414857638
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-07-29 19:54:24 +0000

    Old Collections

---
+++ trunk/Troelsen/Collections/Collections/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayListTest();
+            //ListTest();
+            //StackTest();
+            //BitTest();
+            //QueueTest();
+        }
+
+        private static void QueueTest()
+        {
+            Queue q = new Queue();
+            q.Enqueue("111");
+            q.Enqueue("222");
+            q.Enqueue("333");
+
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Dequeue());
+        }
+
+        private static void BitTest()
+        {
+            BitArray ba = new BitArray(new bool[] { true, 5 < 7, true != false });
+            foreach (bool flag in ba)
+            {
+                Console.WriteLine(flag);
+            }
+        }
+
+        private static void StackTest()
+        {
+            Stack st = new Stack();
+            st.Push(new object());
+            Console.WriteLine(st.Peek());
+            Console.WriteLine(st.Count);
+            Console.WriteLine(st.Pop());
+            Console.WriteLine(st.Count);
+        }
+
+        private static void ListTest()
+        {
+            SortedList sl = new SortedList();
+            sl.Add("test", 44);
+            sl.Add("test2", 55);
+
+            IList keys = sl.GetKeyList();
+            foreach (object key in keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            IList values = sl.GetValueList();
+            foreach (object val in values)
+            {
+                Console.WriteLine(val);
+            }
+        }
+
+        private static void ArrayListTest()
+        {
+            ArrayList al = new ArrayList(5);
+            al.Add(555);
+            al.Add("ddd");
+
+            ArrayList al2 = new ArrayList(2);
+            al2.Add("XXXXXX");
+            al2.Add("YYY");
+            al2.Add("ddfgdfg");
+
+            al.AddRange(al2);
+
+            foreach (object obj in al)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
