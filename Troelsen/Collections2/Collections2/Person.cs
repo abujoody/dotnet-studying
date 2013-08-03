60cae09e145f8fba165856e8ee726583d443b7ba
artemdiatlov@gmail.com <artemdiatlov@gmail.com@b7a331f7-ac55-1738-91f6-8c61a12e126d>
2013-08-03 12:32:07 +0000

    Using Collections.Generic

---
+++ Troelsen/Collections2/Collections2/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("FirstName = {0}, LastName = {1}, Age = {2}",
+                FirstName, LastName, Age);
+        }
+    }
+}
---
+++ Troelsen/Collections2/Collections2/SortPeopleByAge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2
+{
+    class SortPeopleByAge : IComparer<Person>
+    {
+        public int Compare(Person first, Person second)
+        {
+            if (first.Age > second.Age)
+                return 1;
+            if (first.Age < second.Age)
+                return -1;
+
+            return 0;
+        }
+    }
+}
